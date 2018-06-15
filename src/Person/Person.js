6dcb5fcbbc46f18511adffdac8a0c52e09108f29
Tejas Pancholi <tejaser@users.noreply.github.com>
2018-06-15 12:15:15 +0530

    two way binding
    using defaultValue property in input to show two way binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,7 +7,7 @@ const person = props => {
         I'm {props.name}. And I am {props.age} older.
       </p>
       {props.children ? <p>{props.children} </p> : ""}
-      <input type="text" onChange={props.changed} />
+      <input type="text" onChange={props.changed} defaultValue={props.name} />
     </div>
   );
 };
